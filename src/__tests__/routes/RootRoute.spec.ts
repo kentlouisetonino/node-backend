0b5667cd141828fc8e2cee7b1365e634bc59826b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-15 21:55:40 +0800

    Add unit test for RootRoute.

---
+++ src/__tests__/routes/RootRoute.spec.ts
@@ -0,0 +1,8 @@
+import RootRoute from '../../routes/RootRoute';
+
+describe('[src/routes/RootRoute]', () => {
+  test('a. Root type should be a function', (done) => {
+    expect(typeof RootRoute).toEqual('function');
+    done();
+  });
+});
