c50df38d8967df96c804c67a66380fab92ba414d
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-16 09:46:36 +0800

    Add unit test for RootController.

--- src/__tests__/controllers/RootController.spec.ts
+++ src/__tests__/controllers/RootController.spec.ts
@@ -1,1 +1,13 @@
 import RootController from '../../controllers/RootController';
+
+describe('[src/controllers/RootController]', () => {
+  test('a. RootController type should be a function.', (done) => {
+    expect(typeof RootController).toEqual('function');
+    done();
+  });
+
+  test('b. RootController.home type should be a function.', (done) => {
+    expect(typeof RootController.home).toEqual('function');
+    done();
+  });
+});
