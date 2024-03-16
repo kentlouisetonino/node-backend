e5c281fa66d7f57c8b663726dfb03a95b0cd3ca4
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-16 10:08:22 +0800

    Add unit test for OpenAIRoute.

--- src/__tests__/routes/OpenAIRoute.spec.ts
+++ src/__tests__/routes/OpenAIRoute.spec.ts
@@ -3,5 +3,6 @@ import OpenAIRoute from '../../routes/OpenAIRoute';
 describe('[src/routes/OpenAIRoute]', () => {
   test('a. OpenAIRoute type should be a function.', (done) => {
     expect(typeof OpenAIRoute).toEqual('function');
+    done();
   });
 });
