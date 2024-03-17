17e6aed5269090b9e147e02435d4c1b28e774acd
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-17 11:56:58 +0800

    Refactor the AWSService.

--- src/services/AWSService.ts
+++ src/services/AWSService.ts
@@ -1,32 +1,5 @@
 import S3 from 'aws-sdk/clients/s3';
 
-export interface S3UploadFileInterface {
-  bucketName: string;
-  bucketRegion: string;
-  bucketAccessKeyId: string;
-  bucketSecretAccessKey: string;
-  fileName: string;
-  fileBuffer: Buffer;
-  fileEncoding: string;
-  fileContentType: string;
-}
-
-export interface S3DeleteFileInterface {
-  bucketName: string;
-  bucketRegion: string;
-  bucketAccessKeyId: string;
-  bucketSecretAccessKey: string;
-  key: string;
-}
-
-export interface S3GetSignedURLInterface {
-  bucketName: string;
-  bucketRegion: string;
-  bucketAccessKeyId: string;
-  bucketSecretAccessKey: string;
-  key: string;
-}
-
 export default class AWSService {
   static s3UploadFile({
     bucketName,
@@ -37,7 +10,16 @@ export default class AWSService {
     fileBuffer,
     fileEncoding,
     fileContentType,
-  }: S3UploadFileInterface): Promise<S3.ManagedUpload.SendData> {
+  }: {
+    bucketName: string;
+    bucketRegion: string;
+    bucketAccessKeyId: string;
+    bucketSecretAccessKey: string;
+    fileName: string;
+    fileBuffer: Buffer;
+    fileEncoding: string;
+    fileContentType: string;
+  }): Promise<S3.ManagedUpload.SendData> {
     const s3 = new S3({
       region: bucketRegion,
       accessKeyId: bucketAccessKeyId,
@@ -62,7 +44,13 @@ export default class AWSService {
     bucketAccessKeyId,
     bucketSecretAccessKey,
     key,
-  }: S3DeleteFileInterface) {
+  }: {
+    bucketName: string;
+    bucketRegion: string;
+    bucketAccessKeyId: string;
+    bucketSecretAccessKey: string;
+    key: string;
+  }) {
     const s3 = new S3({
       region: bucketRegion,
       accessKeyId: bucketAccessKeyId,
@@ -84,7 +72,13 @@ export default class AWSService {
     bucketAccessKeyId,
     bucketSecretAccessKey,
     key,
-  }: S3GetSignedURLInterface) {
+  }: {
+    bucketName: string;
+    bucketRegion: string;
+    bucketAccessKeyId: string;
+    bucketSecretAccessKey: string;
+    key: string;
+  }) {
     const s3 = new S3({
       region: bucketRegion,
       accessKeyId: bucketAccessKeyId,
